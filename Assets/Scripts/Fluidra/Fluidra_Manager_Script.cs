03a50688d64dd3ffe7547a3b6aefb24a54884c52
gviaud <gviaud@pointcube.fr>
2015-10-23 09:58:11 +0200

    23/10/2015

---
+++ Assets/Scripts/Fluidra/Fluidra_Manager_Script.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fluidra_Manager_Script : MonoBehaviour {
+
+    GameObject spa_GO;
+
+	// Use this for initialization
+	void Start () {
+        spa_GO = transform.FindChild("SM240_Ref").gameObject;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void Change_Shell_Texture()
+    {
+
+
+
+    }
+
+}
---
+++ Assets/Scripts/Fluidra/Spa_Script.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spa_Script : MonoBehaviour {
+
+
+    
+    GameObject shell;
+    GameObject skirt;
+
+
+
+    // Use this for initialization
+    void Start () {
+        shell = transform.FindChild("coque").gameObject;
+        skirt = transform.FindChild("jupe").gameObject;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void Change_Texture(GameObject _gameObject, Texture _tex)
+    {
+        if(_gameObject != null && ( _gameObject == shell || _gameObject == skirt))
+            _gameObject.GetComponent<Renderer>().material.mainTexture = _tex;
+    }
+
+
+    public GameObject GetShell()
+    {
+        return shell;
+    }
+    public GameObject GetSkirt()
+    {
+        return skirt;
+    }
+
+}
