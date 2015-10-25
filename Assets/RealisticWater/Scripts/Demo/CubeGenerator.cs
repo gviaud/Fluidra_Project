bd5b313afa898008a963fa5d58d872193a4dcde6
abdelaziz <abdelaziz.benyahia@esprit.tn>
2015-10-25 13:48:41 +0100

    Luminon

--- Assets/RealisticWater/Scripts/Demo/CubeGenerator.cs
+++ Assets/RealisticWater/Scripts/Demo/CubeGenerator.cs
@@ -3,23 +3,35 @@ using System.Collections;
 
 public class CubeGenerator : MonoBehaviour
 {
-
+	public Camera cam;
     public GameObject cubes;
 	// Use this for initialization
 	void Start () {
-        InvokeRepeating("UpdateCube", 1, 2);
+
 	}
-	
+	void Update(){
+		if (Input.GetButtonDown ("Fire1")) 
+		{
+			var mousePos = Input.mousePosition;
+			mousePos.z = 11;       // we want 2m away from the camera position
+			var objectPos = Camera.main.ScreenToWorldPoint(mousePos);
+			Debug.Log(objectPos);
+			var go = Instantiate(cubes, objectPos, Quaternion.identity)  as GameObject;
+			go.AddComponent<Buoyancy>().Density = Random.Range(700, 850);
+			go.AddComponent<Rigidbody>().mass = Random.Range(100, 150);
+			Destroy(go, 30);
+		}
+	}
 	// Update is called once per frame
 	void UpdateCube ()
 	{
 	    var pos = transform.position;
 	    pos.y += 10;
 	    pos.z -= 4;
-	    pos += Random.insideUnitSphere * 7;
+	    pos += Random.insideUnitSphere *0.01f;
         var go = Instantiate(cubes, pos, Quaternion.Euler(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360))) as GameObject;
-	    go.AddComponent<Buoyancy>().Density = Random.Range(700, 850);
-	    go.AddComponent<Rigidbody>().mass = Random.Range(100, 150);
-        Destroy(go, 30);
+		go.AddComponent<Buoyancy>().Density = Random.Range(700, 850);
+		go.AddComponent<Rigidbody>().mass = Random.Range(100, 150);
+		Destroy(go, 30);
 	}
 }
---
+++ Assets/fixePosLight.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fixePosLight : MonoBehaviour {
+	Vector3 pos;
+
+	void Start () {
+
+	}
+	void update()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter(Collider other){
+
+if (other.name == "plage") {
+			Debug.Log ("D5al");
+			pos=transform.position;
+			GetComponent<Rigidbody>().constraints=RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezePositionY;
+		}
+		if (other.name == "Water") {
+			Debug.Log ("D5al");
+			pos=transform.position;
+			GetComponent<Rigidbody>().constraints=RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ;
+		}
+	}
+
+}
