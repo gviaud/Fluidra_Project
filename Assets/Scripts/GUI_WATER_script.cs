3b255e2c4d58e4a8824b753da259c62d6cf7be50
abdelazizbenyahia <severin1212@gmail.com>
2015-10-26 19:43:08 +0100

    19:43

---
+++ Assets/Scripts/GUI_WATER_script.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GUI_WATER_script : MonoBehaviour {
+
+	GameObject Side_Menu;
+
+	Toggle ToggleLight;
+	Toggle ToggleJupe;
+	
+	// Use this for initialization
+	void Start ()
+	{
+
+		ToggleLight = transform.FindChild("ToggleLight").GetComponent<Toggle>();
+
+		Side_Menu = GameObject.Find("CameraGUI").transform.GetChild(0).FindChild("Background SideMenu").gameObject;
+		
+		//gameObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+		if (ToggleLight.isOn) {
+			Side_Menu.transform.GetChild (2).gameObject.SetActive (true);
+	
+		} 
+		else 
+		{
+			Side_Menu.transform.GetChild (2).gameObject.SetActive (false);
+		}
+
+
+	}
+}
