678e9beecf88e5bb062fddf945e38b5314ef0cdb
abdelaziz <abdelaziz.benyahia@esprit.tn>
2015-10-26 10:07:18 +0100

    a
    a

--- Assets/fixePosLight.cs
+++ Assets/fixePosLight.cs
@@ -25,7 +25,7 @@ public class fixePosLight : MonoBehaviour {
         if (other.name == "Water")
         {
             Debug.Log("D5al");
-			pos = transform.position+ new Vector3(0, -.16f, 0);
+			pos = transform.position+ new Vector3(0, -.162f, 0);
 			transform.position=pos;
 			GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezePositionY;
 		}
